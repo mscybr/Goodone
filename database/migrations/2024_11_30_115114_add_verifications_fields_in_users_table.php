19d287c71e9c490c17d8eadf8462013f314d9edc
Mohamed <mdvlpr1@gmail.com>
2024-11-30 13:52:38 +0200

    verifications-fields

---
+++ database/migrations/2024_11_30_115114_add_verifications_fields_in_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('security_check')->default(false);
+            $table->boolean('verified_liscence')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('security_check');
+            $table->dropColumn('verified_liscence');
+        });
+    }
+};
