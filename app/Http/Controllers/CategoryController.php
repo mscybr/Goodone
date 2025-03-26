19e4fb2d44cc1e8a77dc02149049ccb001d89487
Mohamed <mdvlpr1@gmail.com>
2025-03-26 12:34:12 +0200

    fx2

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
         $categories = Category::Select("id", "name", "image")->With(['Subcategory' => function ($query) {
                 $query->select('id', 'name');
             }])->get();
-        if(Auth("api") !== null){
+        if(Auth("api")->user() != null){
             $user = Auth("api")->user();
             if( $user["type"] == "worker" ){
 
