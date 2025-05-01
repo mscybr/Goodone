2d2b4cfb1b7fa5909c7ec60c5cb3dfb0f78f1d16
Mohamed <mdvlpr1@gmail.com>
2025-05-01 16:36:23 +0200

    tst

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,6 +12,6 @@ class Category extends Model
     protected $guarded = ['id'];
 
     public function Subcategory(){
-        return $this->hasMany(Subcategory::class);
+        return $this->hasMany(Subcategory::class, "id");
     }
 }
