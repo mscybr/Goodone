d37547f1fb72aa8f9d0418462e3d9cf3a5810a72
Mohamed <mdvlpr1@gmail.com>
2025-05-05 09:13:28 +0200

    auth requirement removal from verifyAccount

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', "register", "sendVerificationCode"]]);
+        $this->middleware('auth:api', ['except' => ['login', "register", "sendVerificationCode", "verifyAccount"]]);
     }
 
 
