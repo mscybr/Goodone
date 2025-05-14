4b6c88bb86afc82b2a7163682aa8c00fd4452f77
Mohamed <mdvlpr1@gmail.com>
2025-05-14 11:58:31 +0200

    fx

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -185,8 +185,8 @@ class AdminController extends Controller
             $orders = Order::join('services', "services.id", "=", "order.service_id")->select("services.*", "order.*")->Where( [["services.user_id", "=", $user->id], ["order.status", ">", 0]])->orderBy('orders.updated_at','DESC')->get();
             $withdrawals = WithdrawRequest::Where([["status", "<", "2"]])->orderBy('updated_at','DESC')->get();
             $merged_dates_array = [];
-            foreach ($orders as $key => $order ) $merged_dates_array[] = ["type"=> "order", "values" => $order, "date" => $order->updated_at];
-            foreach ($withdrawals as $key => $withdrawal ) $merged_dates_array[] = ["type"=> "withdrawal", "values" => $withdrawal, "date" => $withdrawal->updated_at];
+            foreach ($orders as $order ) $merged_dates_array[] = ["type"=> "order", "values" => $order, "date" => $order->updated_at];
+            foreach ($withdrawals as $withdrawal ) $merged_dates_array[] = ["type"=> "withdrawal", "values" => $withdrawal, "date" => $withdrawal->updated_at];
             usort($merged_dates_array, fn($a, $b) => $a['date'] <=> $b['date']);
             $total_transactions = [];
             foreach ($merged_dates_array as $item ) {
