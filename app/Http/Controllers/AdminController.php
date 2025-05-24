381ff3414e9c80734146f7ab21223f0cd13d8d65
Mohamed <mdvlpr1@gmail.com>
2025-05-24 14:16:10 +0200

    normal

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -140,13 +140,13 @@ class AdminController extends Controller
             $stats_month["earnings_difference"] = $stats_month["earnings"] * 100;
         }
 
-        dd([
-            "month_stats" => $stats_month,
-            "day_stats" => $stats_day,
-            "stats_year" => $stats_year,
-            "stats_yesterday" => $stats_yesterday,
-            "stats_past_month" => $stats_past_month
-        ]);
+        // dd([
+        //     "month_stats" => $stats_month,
+        //     "day_stats" => $stats_day,
+        //     "stats_year" => $stats_year,
+        //     "stats_yesterday" => $stats_yesterday,
+        //     "stats_past_month" => $stats_past_month
+        // ]);
         return view("admin.index", [
             "month_stats" => $stats_month,
             "day_stats" => $stats_day,
