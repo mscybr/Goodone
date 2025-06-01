cae37af1f9b580d49cff4d364ec20e99c4cdca68
Mohamed <mdvlpr1@gmail.com>
2025-06-01 12:30:51 +0200

    METHOD POST

--- resources/views/admin/region_taxes.blade.php
+++ resources/views/admin/region_taxes.blade.php
@@ -83,7 +83,7 @@
     }
      function formcreator(action_url, name, current_value){
       let inputs = `<input class="form-control me-3" name="${name}" placeholder="${current_value}">`;
-      return `<form action="${action_url}" target="dummyframe" class="d-flex">
+      return `<form method="POST" action="${action_url}" target="dummyframe" class="d-flex">
         ${inputs}
        <button ondblclick=submit_form(this) class="btn btn-success">Edit</button>
       </form>`
